6d906d204f03db44cb9cf8cf4a3fba44f71b84a6
Gabriel Przybysz <45472156+GabrielPrzybysz@users.noreply.github.com>
2020-02-27 15:57:52 -0300

    Movement script more legible

--- TypeoProject/Assets/Scripts/Movement.cs
+++ TypeoProject/Assets/Scripts/Movement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,7 +10,7 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A)) 
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Move(new Vector2Int(-1, 0), GridConstructor.PlayerPosition);
             SoundManager.PlaySingle(MovementAudio);
@@ -32,64 +32,85 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void Move(Vector2Int nextPos, Vector2Int currentPos) 
+    private void Move(Vector2Int nextPos, Vector2Int currentPos)
     {
-        if (currentPos.x + nextPos.x >= 0 && currentPos.x + nextPos.x < GridConstructor.accessWidth && currentPos.y + nextPos.y >= 0 && currentPos.y + nextPos.y < GridConstructor.accessHeight) 
+        if (currentPos.x + nextPos.x >= 0 && currentPos.x + nextPos.x < GridConstructor.accessWidth)
         {
-            if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Free)
+            if (currentPos.y + nextPos.y >= 0)
             {
-                if (GridConstructor.Matrix[currentPos.x, currentPos.y] == (int)GridConstructor.Cells.PlayerOnPoint)
+                if (currentPos.y + nextPos.y < GridConstructor.accessHeight)
                 {
-                    GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Point;
+                    if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Free)
+                    {
+                        if (GridConstructor.Matrix[currentPos.x, currentPos.y] == (int)GridConstructor.Cells.PlayerOnPoint)
+                        {
+                            GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Point;
+                        }
+                        else
+                        {
+                            GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
+                        }
+                        GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.Player;
+                        GridConstructor.PlayerPosition = currentPos + nextPos;
+                    }
+                    else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.OnPoint)
+                    {
+                        if (GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Wall)
+                        {
+                            if (GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Item)
+                            {
+                                GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
+                                GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.PlayerOnPoint;
+                                GridConstructor.PlayerPosition = currentPos + nextPos;
+                                MoveItem(nextPos, nextPos + currentPos);
+                            }
+                        }
+                    }
+                    else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Item)
+                    {
+                        if (GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Wall)
+                        {
+                            if (GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Item)
+                            {
+                                if (GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.OnPoint)
+                                {
+                                    MoveItem(nextPos, nextPos + currentPos);
+                                    Move(nextPos, currentPos);
+                                }
+                            }
+                        }
+                    }
+                    if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Point)
+                    {
+                        if (GridConstructor.Matrix[currentPos.x, currentPos.y] == (int)GridConstructor.Cells.PlayerOnPoint)
+                        {
+                            GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Point;
+                        }
+                        else
+                        {
+                            GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
+                        }
+                        GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.PlayerOnPoint;
+                        GridConstructor.PlayerPosition = currentPos + nextPos;
+                    }
                 }
-                else 
-                {
-                    GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
-                }
-                GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.Player;
-                GridConstructor.PlayerPosition = currentPos + nextPos;
-            }
-            else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.OnPoint && GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Wall && GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Item)
-            {
-                GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
-                GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int) GridConstructor.Cells.PlayerOnPoint;
-                GridConstructor.PlayerPosition = currentPos + nextPos;
-                MoveItem(nextPos, nextPos + currentPos);
-            }
-            else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Item && GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int) GridConstructor.Cells.Wall && GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.Item && GridConstructor.Matrix[nextPos.x + nextPos.x * 1 + currentPos.x, nextPos.y + nextPos.y * 1 + currentPos.y] != (int)GridConstructor.Cells.OnPoint)
-            {
-                MoveItem(nextPos, nextPos + currentPos);
-                Move(nextPos, currentPos);
-            }
-            if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Point)
-            {
-                if (GridConstructor.Matrix[currentPos.x, currentPos.y] == (int)GridConstructor.Cells.PlayerOnPoint)
-                {
-                    GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Point;
-                }
-                else
-                {
-                    GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
-                }
-                GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.PlayerOnPoint;
-                GridConstructor.PlayerPosition = currentPos + nextPos;
             }
         }
     }
 
-    private void MoveItem(Vector2Int nextPos, Vector2Int currentPos) 
+    private void MoveItem(Vector2Int nextPos, Vector2Int currentPos)
     {
-        if (currentPos.x + nextPos.x >= 0 && currentPos.x + nextPos.x < GridConstructor.accessWidth && currentPos.y + nextPos.y >= 0 && currentPos.y + nextPos.y < GridConstructor.accessHeight) 
+        if (currentPos.x + nextPos.x >= 0 && currentPos.x + nextPos.x < GridConstructor.accessWidth && currentPos.y + nextPos.y >= 0 && currentPos.y + nextPos.y < GridConstructor.accessHeight)
         {
             if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Free)
             {
-                if (GridConstructor.Matrix[currentPos.x, currentPos.y] != (int)GridConstructor.Cells.PlayerOnPoint) 
+                if (GridConstructor.Matrix[currentPos.x, currentPos.y] != (int)GridConstructor.Cells.PlayerOnPoint)
                 {
                     GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
                 }
                 GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.Item;
             }
-            else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int) GridConstructor.Cells.Point)
+            else if (GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] == (int)GridConstructor.Cells.Point)
             {
                 GridConstructor.Matrix[currentPos.x, currentPos.y] = (int)GridConstructor.Cells.Free;
                 GridConstructor.Matrix[nextPos.x + currentPos.x, nextPos.y + currentPos.y] = (int)GridConstructor.Cells.OnPoint;
